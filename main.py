7c75d79cfef9526a16bfb6bb02ad09645d28fca5
Pavel Šimon <Pavel@pavel-simon.com>
2020-12-25 13:27:03 +0100

    Prvý nástrel pre "device" a úprava starého sajtu

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import uvicorn
 import requests
 import time
 
-url = 'http://dealan.sk/json3.php'
 
 app = FastAPI()
 
@@ -20,22 +19,27 @@ async def root(request: Request):
     """
     Show dashboard of all sensors from 'http://dealan.sk/json3.php'
     """
+    url = 'http://dealan.sk/json3.php'
     response = requests.get(url)
     zdroje = response.json()
     localtime = time.asctime(time.localtime(time.time()))
     return templates.TemplateResponse("home.html", {"request": request, "zdroje": zdroje, "time": localtime})
 
 
-@app.get("/{triedenie}", response_class=HTMLResponse)
+@app.get("/triedenie/{triedenie}", response_class=HTMLResponse)
 async def sorted_root(request: Request, triedenie: str):
     """
     Show dashboard of all sensors (sorted) from 'http://dealan.sk/json3.php'
     """
     def myFunc(e):
         return e[triedenie]
+    url = 'http://dealan.sk/json3.php'
     response = requests.get(url)
     zdroje = response.json()
-    #print("Triedenie:", triedenie, "je typu:", type(triedenie))
+    # zdrojes = sorted(zdroje)
+    # print(zdrojes)
+    print("zdroje[0]['id']:", zdroje[0]['id'],
+          "je typu:", type(zdroje[0]['id']))
     zdroje.sort(key=myFunc)
     localtime = time.asctime(time.localtime(time.time()))
     return templates.TemplateResponse("home.html", {"request": request, "triedenie": triedenie, "zdroje": zdroje, "time": localtime})
@@ -46,8 +50,20 @@ async def read_item(request: Request, id: str):
     """
     show information about one particulal device (sensor)
     """
-    return templates.TemplateResponse("device.html", {"request": request, "id": id})
+    url = 'http://dealan.sk/device.php'
+    response = requests.get(url)
+    zdroje = response.json()
+    localtime = time.asctime(time.localtime(time.time()))
+    return templates.TemplateResponse("device.html", {"request": request, "id": id, "zdroje": zdroje, "time": localtime})
+
+
+@app.get("/old")
+async def old(request: Request):
+    """
+    show inline old php web
+    """
+    return templates.TemplateResponse("old.html", {"request": request})
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(
-        PORT), reload=True, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(PORT), reload=True, debug=True)
