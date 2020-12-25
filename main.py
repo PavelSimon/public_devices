047d471570d45c1b6a5878e1e795abb48d332a31
Pavel Šimon <pavel@pavel-simon.com>
2020-12-25 20:55:08 +0100

    úprava zvýraznenia "starých" zdrojov

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 import requests
 import time
-
+from dateutil.parser import parse
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 templates = Jinja2Templates(directory="./templates")
 
+
+def nacitaj_udaje():
+    url = 'http://dealan.sk/json3.php'
+    response = requests.get(url)
+    zdroje = response.json()
+    localtime_int = time.time()
+    for zdroj in zdroje:
+        device_time = parse(zdroj['naposledy'])
+        #print(localtime_int - device_time.timestamp())
+        zdroj["time_diff"] = localtime_int - device_time.timestamp()
+        # print(zdroj)
+    return zdroje
 # Routes:
 
 
@@ -21,9 +33,7 @@ async def root(request: Request):
     """
     Show dashboard of all sensors from 'http://dealan.sk/json3.php'
     """
-    url = 'http://dealan.sk/json3.php'
-    response = requests.get(url)
-    zdroje = response.json()
+    zdroje = nacitaj_udaje()
     localtime = time.asctime(time.localtime(time.time()))
     return templates.TemplateResponse("home.html", {"request": request, "zdroje": zdroje, "time": localtime})
 
@@ -35,13 +45,8 @@ async def sorted_root(request: Request, triedenie: str):
     """
     def myFunc(e):
         return e[triedenie]
-    url = 'http://dealan.sk/json3.php'
-    response = requests.get(url)
-    zdroje = response.json()
-    # zdrojes = sorted(zdroje)
-    # print(zdrojes)
-    # print("zdroje[0]['id']:", zdroje[0]['id'],
-    #       "je typu:", type(zdroje[0]['id']))
+
+    zdroje = nacitaj_udaje()
     zdroje.sort(key=myFunc)
     localtime = time.asctime(time.localtime(time.time()))
     return templates.TemplateResponse("home.html", {"request": request, "triedenie": triedenie, "zdroje": zdroje, "time": localtime})
@@ -50,10 +55,9 @@ async def sorted_root(request: Request, triedenie: str):
 @app.get("/device/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     """
-    show information about one particulal device (sensor)
+    show information about one particural device (sensor)
     """
     url = 'http://dealan.sk/device.php?id='+id
-    print(url)
     response = requests.get(url)
     zdroj = response.json()  # '{"id": 1}'  #
     localtime = time.asctime(time.localtime(time.time()))
